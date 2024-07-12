34ffa574163b4408e7ac4a079eb435b79bfc8e8e
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-07-12 21:31:58 +0700

    Changed some view functions to view classes

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
-def index(request):
-    return render(request, 'articles/index.html', context={
-        'app_name': 'Articles'
-    })
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'articles/index.html', context={
+            'app_name': 'Articles'
+        })
--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -20,7 +20,7 @@ from hexlet_django_blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.IndexView.as_view()),
     path('about/', views.about),
     path('articles/', include('hexlet_django_blog.article.urls')),
 ]
