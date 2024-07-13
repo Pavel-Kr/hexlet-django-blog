d8ffae576705c0c5f762f5feda89816a3fa472bf
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-07-13 15:13:12 +0700

    Add dynamic article URL

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 
 # Create your views here.
-class IndexView(View):
+class index(View):
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'articles/index.html', context={
-            'app_name': 'Articles'
-        })
+        return redirect(reverse('article', kwargs={
+            'tag': 'python',
+            'article_id': 42
+        }))
+    
+
+def article(request, tag, article_id):
+    return render(
+        request,
+        'articles/index.html',
+        context={
+            'tag': tag,
+            'article_id': article_id
+        }
+    )
