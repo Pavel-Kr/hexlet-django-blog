2eace3a93767a7ac9f5c784325b278db1df32878
Pavel Krivosheev <pkrivosheev@bk.ru>
2024-07-14 21:15:50 +0700

    Add article creation form and flash system

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.contrib.messages import constants as msg_level
 from django.urls import reverse
 from django.views import View
 
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
 
 
 class IndexView(View):
@@ -20,4 +23,23 @@ class ArticleView(View):
         article = get_object_or_404(Article, id=kwargs['id'])
         return render(request, 'articles/show.html', context={
             'article': article
+        })
+
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', context={
+            'form': form
+        })
+    
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Статья успешно добавлена')
+            return redirect('articles')
+        return render(request, 'articles/create.html', context={
+            'form': form
         })
